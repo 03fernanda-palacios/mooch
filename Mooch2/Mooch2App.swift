c50290d70ee51297bbe2aa1743d432e85e581d79
03fernanda-palacios <03fernanda.palacios@gmail.com>
2026-05-10 00:54:20 -0700

    Initial Commit

---
+++ Mooch2/Mooch2App.swift
@@ -0,0 +1,17 @@
+//
+//  Mooch2App.swift
+//  Mooch2
+//
+//  Created by Fernanda Palacios on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Mooch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
